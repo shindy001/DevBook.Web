9fc200cbac9964d7e916c306c7d7e744c943edc6
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-19 23:16:59 +0100

    feat: introduce share Entity record

---
+++ src/DevBook.Web.Shared/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace DevBook.Web.Shared;
+
+/// <summary>
+/// Represent Entity with id - base for entities with identifier
+/// </summary>
+/// <param name="Id">Identifier</param>
+public abstract record Entity(Guid Id);

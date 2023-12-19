2a3e0828995ab561c65aed071e2cc38d854d229b
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-20 00:08:04 +0100

    feat(ApiService): implement UnitOfWorkCommandPipelineBehavior

---
+++ src/DevBook.Web.ApiService/Infrastructure/UnitOfWorkCommandPipelineBehavior.cs
@@ -0,0 +1,20 @@
+﻿using DevBook.Web.Shared.Contracts;
+using MediatR;
+
+namespace DevBook.Web.ApiService.Infrastructure;
+
+internal sealed class UnitOfWorkCommandPipelineBehavior<TCommand, TResult>
+	: ICommandPipelineBehavior<TCommand, TResult>
+	where TCommand : ICommandBase
+{
+	private readonly IUnitOfWork _unitOfWork;
+
+	public UnitOfWorkCommandPipelineBehavior(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+	public async Task<TResult> Handle(TCommand request, RequestHandlerDelegate<TResult> next, CancellationToken cancellationToken)
+	{
+		var response = await next();
+		await _unitOfWork.CommitAsync(cancellationToken);
+		return response;
+	}
+}

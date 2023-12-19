bac47f0c4dee1130db63a098b007e8e75de1740f
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-19 23:27:42 +0100

    feat: add DI extensions for executor

---
+++ src/DevBook.Web.Shared/ExecutorDependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using DevBook.Web.Shared.Contracts;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace DevBook.Web.Shared;
+
+public static class ExecutorDependencyInjection
+{
+	public static IServiceCollection AddCommandsAndQueriesExecutor(this IServiceCollection services, params Assembly[] commandAndQueriesAssemblies)
+	{
+		services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(commandAndQueriesAssemblies));
+		services.AddScoped<IExecutor, Executor>();
+
+		return services;
+	}
+
+	public static IServiceCollection AddPipelineBehavior(this IServiceCollection services, Type pipelineBehavior)
+	{
+		services.AddScoped(typeof(IPipelineBehavior<,>), pipelineBehavior);
+
+		return services;
+	}
+}

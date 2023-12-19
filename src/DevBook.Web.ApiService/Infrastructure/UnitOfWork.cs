197d9f3cc276f91e9993d2d76e5aef7e58dec244
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-20 00:07:35 +0100

    feat(ApiService): implement UnitOfWork

---
+++ src/DevBook.Web.ApiService/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DevBook.Web.Shared.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBook.Web.ApiService.Infrastructure;
+
+internal sealed class UnitOfWork(DevBookDbContext _devBookDbContext) : IUnitOfWork
+{
+	public void AsNoTrackingQuery()
+	{
+		_devBookDbContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+	}
+
+	public async Task<int> CommitAsync(CancellationToken cancellationToken = default)
+		=> _devBookDbContext.ChangeTracker.QueryTrackingBehavior is QueryTrackingBehavior.NoTracking
+			? throw new InvalidOperationException("Cannot call CommitAsync when NoTracking behavior is set")
+			: await _devBookDbContext.SaveChangesAsync(cancellationToken);
+}

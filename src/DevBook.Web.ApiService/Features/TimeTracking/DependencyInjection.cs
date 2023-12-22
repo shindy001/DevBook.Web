07d67c49767cfe752d4464027d22f01b0dabf933
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-22 22:21:59 +0100

    feat(ApiService): introduce TimeTracking feature DI stubs

---
+++ src/DevBook.Web.ApiService/Features/TimeTracking/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace DevBook.Web.ApiService.Features.TimeTracking;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection RegisterTimeTrackingServices(this IServiceCollection services)
+	{
+		return services;
+	}
+
+	public static IEndpointRouteBuilder MapTimeTrackingEndpoints(this IEndpointRouteBuilder endpoints)
+	{
+		return endpoints;
+	}
+}
--- src/DevBook.Web.ApiService/Program.cs
+++ src/DevBook.Web.ApiService/Program.cs
@@ -1,3 +1,4 @@
+using DevBook.Web.ApiService.Features.TimeTracking;
 using DevBook.Web.ApiService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,14 +10,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure();
+builder.Services.RegisterTimeTrackingServices();
 
 var app = builder.Build();
 
 // Create DB if not exist or migrate if not up to date
 app.InitializeDb(applyMigrations: true);
-
-// Health check + Alive
-app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
@@ -25,5 +24,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Health check + Alive
+app.MapDefaultEndpoints();
+
+app.MapTimeTrackingEndpoints();
 
 app.Run();

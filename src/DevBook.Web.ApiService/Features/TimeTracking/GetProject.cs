6b1d447405a268adc1be2f62ba08dfe17769133d
shindy001 <sindelar.p.etr@seznam.cz>
2023-12-22 23:04:44 +0100

    feat(ApiService): introduce GetProject and GetProjects queries

---
+++ src/DevBook.Web.ApiService/Features/TimeTracking/GetProject.cs
@@ -0,0 +1,20 @@
+﻿using DevBook.Web.ApiService.Infrastructure;
+using DevBook.Web.Shared.Contracts;
+using OneOf;
+using OneOf.Types;
+
+namespace DevBook.Web.ApiService.Features.TimeTracking;
+
+internal record GetProjectQuery(Guid Id) : IQuery<OneOf<Project, NotFound>>;
+
+internal class GetProjectQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProjectQuery, OneOf<Project, NotFound>>
+{
+	public async Task<OneOf<Project, NotFound>> Handle(GetProjectQuery request, CancellationToken cancellationToken)
+	{
+		var project = await dbContext.Projects.FindAsync([request.Id], cancellationToken);
+
+		return project is null
+			? new NotFound()
+			: project;
+	}
+}
---
+++ src/DevBook.Web.ApiService/Features/TimeTracking/GetProjects.cs
@@ -0,0 +1,16 @@
+﻿using DevBook.Web.ApiService.Infrastructure;
+using DevBook.Web.Shared.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevBook.Web.ApiService.Features.TimeTracking;
+
+internal sealed record GetProjectsQuery : IQuery<IEnumerable<Project>>;
+
+internal sealed class GetProjectsQueryHandler(DevBookDbContext dbContext) : IQueryHandler<GetProjectsQuery, IEnumerable<Project>>
+{
+	public async Task<IEnumerable<Project>> Handle(GetProjectsQuery request, CancellationToken cancellationToken)
+	{
+		// TODO - implement paging
+		return await dbContext.Projects.ToListAsync(cancellationToken);
+	}
+}

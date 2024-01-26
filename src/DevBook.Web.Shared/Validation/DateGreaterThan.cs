794f1fb0190526b1643e0549524e4ba9c3d897d9
shindy001 <sindelar.p.etr@seznam.cz>
2024-01-26 18:42:47 +0100

    feat: introduce shared validation attribute TimeSpanGreaterThan

---
+++ src/DevBook.Web.Shared/Validation/DateGreaterThan.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevBook.Web.Shared.Validation;
+public class TimeSpanGreaterThanAttribute : ValidationAttribute
+{
+	private readonly string _comparisonProperty;
+
+	public TimeSpanGreaterThanAttribute(string comparisonProperty)
+	{
+		_comparisonProperty = comparisonProperty;
+	}
+
+	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+	{
+		if (value is null)
+		{
+			return ValidationResult.Success;
+		}
+
+		var currentTimeSpan = value as TimeSpan? ?? throw new InvalidOperationException("Annotated property is not a DateTime");
+		var comparisonValue = validationContext?.ObjectType?.GetProperty(_comparisonProperty)?.GetValue(validationContext.ObjectInstance) as TimeSpan?;
+
+		if (comparisonValue is null || currentTimeSpan < comparisonValue)
+		{
+			return new ValidationResult(ErrorMessage = "End time must be later than start time");
+		}
+
+		return ValidationResult.Success;
+	}
+}

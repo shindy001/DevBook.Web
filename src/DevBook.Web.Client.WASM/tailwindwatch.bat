a2ab62d6f13f1c562defbcd33d7e83567fd63d18
shindy001 <sindelar.p.etr@seznam.cz>
2024-02-23 22:28:54 +0100

    refactor(Client): remove tailwind executables, add actual generated css

--- src/DevBook.Web.Client.WASM/tailwindwatch.bat
+++ src/DevBook.Web.Client.WASM/tailwindwatch.bat
@@ -1,2 +1,2 @@
-:: Check if tailwindcss.exe is running -> if not, start it in watch mode for hot reload
-TASKLIST | FINDSTR tailwindcss.exe || tailwindcss.exe -i .\Styles\tailwind.css -o .\wwwroot\tailwind.css --watch
+:: Check if tailwindcss is running -> if not, start it in watch mode for hot reload
+TASKLIST | FINDSTR tailwindcss || tailwindcss -i .\Styles\tailwind.css -o .\wwwroot\tailwind.css --watch

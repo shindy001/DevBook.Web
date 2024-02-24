751526f2e9f387f29dc6a6aabfe30294751df31e
shindy001 <sindelar.p.etr@seznam.cz>
2024-02-24 10:31:22 +0100

    refactor(Client): separate wwwroot css and images to folders

--- src/DevBook.Web.Client.WASM/tailwindwatch.bat
+++ src/DevBook.Web.Client.WASM/tailwindwatch.bat
@@ -1,2 +1,2 @@
 :: Check if tailwindcss is running -> if not, start it in watch mode for hot reload
-TASKLIST | FINDSTR tailwindcss || tailwindcss -i .\Styles\tailwind.css -o .\wwwroot\tailwind.css --watch
+TASKLIST | FINDSTR tailwindcss || tailwindcss -i .\Styles\tailwind.css -o .\wwwroot\css\tailwind\tailwind.css --watch

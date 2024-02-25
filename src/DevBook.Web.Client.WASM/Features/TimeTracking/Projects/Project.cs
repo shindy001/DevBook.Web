5f95d630ef021b70082d186070a0b3dd267cf8da
shindy001 <sindelar.p.etr@seznam.cz>
2024-02-25 21:36:30 +0100

    feat(Client): add project selectbox to create/update task forms

--- src/DevBook.Web.Client.WASM/Features/TimeTracking/Projects/Project.cs
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Projects/Project.cs
@@ -2,7 +2,7 @@
 
 namespace DevBook.Web.Client.WASM.Features.TimeTracking.Projects;
 
-public sealed record Project(Guid Id, string Name, int? HourlyRate, string? Currency, string? HexColor)
+public sealed record Project(Guid Id, string Name, int? HourlyRate = null, string? Currency = null, string? HexColor = null)
 {
 	public static Project FromDto(ProjectDto dto) => new(dto.Id, dto.Name, dto.HourlyRate, dto.Currency, dto.HexColor);
 }

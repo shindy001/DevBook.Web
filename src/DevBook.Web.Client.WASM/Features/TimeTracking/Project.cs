9f3914059241e1fe540f201416e9b687b782cded
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-06 01:24:24 +0200

    refactor: extract GetProject grapql query to GetProject Query

--- src/DevBook.Web.Client.WASM/Features/TimeTracking/Project.cs
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Project.cs
@@ -1,6 +1,6 @@
 ﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking;
 
-public sealed record Project(Guid Id, string Name, int? HourlyRate = null, string? Currency = null, string? HexColor = null)
+public sealed record Project(Guid Id, string Name, string? Details, int? HourlyRate = null, string? Currency = null, string? HexColor = null)
 {
-	public static Project FromDto(ProjectDto dto) => new(dto.Id, dto.Name, dto.HourlyRate, dto.Currency, dto.HexColor);
+	public static Project FromDto(ProjectDto dto) => new(dto.Id, dto.Name, dto.Details, dto.HourlyRate, dto.Currency, dto.HexColor);
 }
---
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Projects/Queries/GetProject.cs
@@ -0,0 +1,23 @@
+﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking.Projects.Queries;
+
+internal static class GetProject
+{
+	internal sealed record Query(Guid Id) : IRequest<OneOf<Project, DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Query, OneOf<Project, DevBookError>>
+	{
+		public async Task<OneOf<Project, DevBookError>> Handle(Query request, CancellationToken cancellationToken)
+		{
+			var result = await client.GetProject.ExecuteAsync(new() { Id = request.Id }, cancellationToken);
+			var project = result.Data?.Project as GetProject_Project_ProjectDto;
+
+			if (result.IsErrorResult() || project is null)
+			{
+				return result.CreateError();
+			}
+
+			return result.Unwrap(() => new Project(project.Id, project.Name, project.Details, project.HourlyRate, project.Currency, project.HexColor));
+		}
+	}
+}
+

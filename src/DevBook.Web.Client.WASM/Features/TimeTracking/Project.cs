f1b03502c4c75b7c48a797fc4955968fdae5f474
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-07 00:29:09 +0200

    feat: introduce GetProjects grapqhl query and handler

--- src/DevBook.Web.Client.WASM/Features/TimeTracking/Project.cs
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Project.cs
@@ -3,4 +3,6 @@
 public sealed record Project(Guid Id, string Name, string? Details, int? HourlyRate = null, string? Currency = null, string? HexColor = null)
 {
 	public static Project FromDto(ProjectDto dto) => new(dto.Id, dto.Name, dto.Details, dto.HourlyRate, dto.Currency, dto.HexColor);
+
+	public override string ToString() => Name;
 }
---
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Projects/Queries/GetProjects.cs
@@ -0,0 +1,23 @@
+﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking.Projects.Queries;
+
+internal static class GetProjects
+{
+	internal sealed record Query : IRequest<OneOf<IEnumerable<Project>, DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Query, OneOf<IEnumerable<Project>, DevBookError>>
+	{
+		public async Task<OneOf<IEnumerable<Project>, DevBookError>> Handle(Query request, CancellationToken cancellationToken)
+		{
+			var result = await client.GetProjects.ExecuteAsync(cancellationToken);
+			var projects = result.Data?.Projects;
+
+			if (result.IsErrorResult() || projects is null)
+			{
+				return result.CreateError();
+			}
+
+			return result.Unwrap(() => projects.Select(x => new Project(x.Id, x.Name, x.Details, x.HourlyRate, x.Currency, x.HexColor)));
+		}
+	}
+}
+

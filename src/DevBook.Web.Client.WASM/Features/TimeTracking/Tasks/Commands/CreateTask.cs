4243ccb30716fb4c28b3108cbac4fb605aee074c
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-07 00:29:39 +0200

    feat: introduce CreateTask graphql mutation and handler

---
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Tasks/Commands/CreateTask.cs
@@ -0,0 +1,33 @@
+﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking.Tasks.Commands;
+
+internal static class CreateTask
+{
+	internal sealed record Command(
+		Guid? ProjectId,
+		string? Description,
+		string? Details,
+		DateTimeOffset Date,
+		TimeSpan Start,
+		TimeSpan End)
+		: IRequest<OneOf<Success, DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Command, OneOf<Success, DevBookError>>
+	{
+		public async Task<OneOf<Success, DevBookError>> Handle(Command request, CancellationToken cancellationToken)
+		{
+			var result = await client.CreateTask.ExecuteAsync(
+					new()
+					{
+						ProjectId = request.ProjectId,
+						Description = request.Description,
+						Details = request.Details,
+						Date = request.Date,
+						Start = request.Start,
+						End = request.End
+					},
+				cancellationToken);
+
+			return result.Unwrap();
+		}
+	}
+}
--- src/DevBook.Web.Client.WASM/Features/TimeTracking/WorkTask.cs
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/WorkTask.cs
@@ -1,11 +1,12 @@
 ﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking;
 
-public sealed record WorkTask(Guid Id, Project? Project, string? Description, DateTimeOffset Date, TimeSpan Start, TimeSpan? End)
+public sealed record WorkTask(Guid Id, Project? Project, string? Description, string? Details, DateTimeOffset Date, TimeSpan Start, TimeSpan? End)
 {
 	public static WorkTask FromDto(WorkTaskDto dto) => new(
 		dto.Id,
 		dto.Project is null ? null : Project.FromDto(dto.Project),
 		dto.Description,
+		dto.Details,
 		dto.Date,
 		dto.Start,
 		dto.End);

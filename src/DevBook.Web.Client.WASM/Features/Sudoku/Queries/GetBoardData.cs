acb38a05baa340e2d57f382aaf51bd8532ee1909
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-06 21:28:43 +0200

    refactor: convert GetBoardData api call to grapql query and handler

---
+++ src/DevBook.Web.Client.WASM/Features/Sudoku/Queries/GetBoardData.cs
@@ -0,0 +1,26 @@
+﻿namespace DevBook.Web.Client.WASM.Features.Sudoku.Queries;
+
+internal static class GetBoardData
+{
+	internal sealed record Query : IRequest<OneOf<int[][], DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Query, OneOf<int[][], DevBookError>>
+	{
+		public async Task<OneOf<int[][], DevBookError>> Handle(Query request, CancellationToken cancellationToken)
+		{
+			var result = await client.GetBoardData.ExecuteAsync(cancellationToken);
+			var boardData = result.Data?.BoardData;
+
+			if (result.IsErrorResult() || boardData is null)
+			{
+				return result.CreateError();
+			}
+
+			var gridNumbersArray = boardData.GridNumbers
+			   .Select(innerCollection => innerCollection.ToArray())
+			   .ToArray();
+
+			return result.Unwrap(() => gridNumbersArray);
+		}
+	}
+}

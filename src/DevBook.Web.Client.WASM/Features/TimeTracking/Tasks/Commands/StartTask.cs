2611f4301d1cb6b81fd80ebfd77e077837024d74
shindy001 <sindelar.p.etr@seznam.cz>
2024-05-09 23:04:01 +0200

    refactor: extract and convert start/stop task to graphql commands

---
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Tasks/Commands/StartTask.cs
@@ -0,0 +1,27 @@
+﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking.Tasks.Commands;
+
+internal static class StartTask
+{
+	internal sealed record Command(
+		string? Description,
+		DateTimeOffset Date,
+		TimeSpan Start)
+		: IRequest<OneOf<Success, DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Command, OneOf<Success, DevBookError>>
+	{
+		public async Task<OneOf<Success, DevBookError>> Handle(Command request, CancellationToken cancellationToken)
+		{
+			var result = await client.StartTask.ExecuteAsync(
+					new()
+					{
+						Description = request.Description,
+						Date = request.Date,
+						Start = request.Start,
+					},
+				cancellationToken);
+
+			return result.Unwrap();
+		}
+	}
+}
---
+++ src/DevBook.Web.Client.WASM/Features/TimeTracking/Tasks/Commands/StopTask.cs
@@ -0,0 +1,25 @@
+﻿namespace DevBook.Web.Client.WASM.Features.TimeTracking.Tasks.Commands;
+
+internal static class StopTask
+{
+	internal sealed record Command(
+		Guid Id,
+		TimeSpan End)
+		: IRequest<OneOf<Success, DevBookError>>;
+
+	private sealed class Handler(IDevBookWebApiGraphQLClient client) : IRequestHandler<Command, OneOf<Success, DevBookError>>
+	{
+		public async Task<OneOf<Success, DevBookError>> Handle(Command request, CancellationToken cancellationToken)
+		{
+			var result = await client.StopTask.ExecuteAsync(
+					new()
+					{
+						Id = request.Id,
+						End = request.End
+					},
+				cancellationToken);
+
+			return result.Unwrap();
+		}
+	}
+}
